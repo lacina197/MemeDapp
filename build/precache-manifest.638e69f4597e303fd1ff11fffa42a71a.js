4bcf22f7279e0b9a014ab808969d1ec192512872
lacina197 <lacinacaroglu@gmail.com>
2021-12-23 13:29:11 +0300

    build dir is added.

---
+++ build/precache-manifest.638e69f4597e303fd1ff11fffa42a71a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c35017bcf95e2819876",
+    "url": "/static/css/main.6addba20.chunk.css"
+  },
+  {
+    "revision": "8c35017bcf95e2819876",
+    "url": "/static/js/main.8c35017b.chunk.js"
+  },
+  {
+    "revision": "3f8fb9368e91d0666682",
+    "url": "/static/css/1.31c29d01.chunk.css"
+  },
+  {
+    "revision": "3f8fb9368e91d0666682",
+    "url": "/static/js/1.3f8fb936.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "22a5ef7a19d46026db65d6e2a13c2450",
+    "url": "/index.html"
+  }
+];
